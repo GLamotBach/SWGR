da08ee9724f2a7284c83f8ca399e94c8ed65cd2c
Grzegorz Lamot-Bach <105417272+stockphotohacker@users.noreply.github.com>
2024-07-01 12:11:42 +0200

    GUI pt.2

--- gui.py
+++ gui.py
@@ -1,10 +1,14 @@
 import tkinter as tk
-# from PIL import Image, ImageTk
-# Pillow not yet installed
+from PIL import Image, ImageTk
+
+from user_interface import appraise_offer_approx
 
 
-def test_function():
-    appraise_text.set("loading")
+def button_function():
+    """Functionality of the GUI, appraise (pol:'Wyceń') button."""
+    url = entry.get()
+    price = appraise_offer_approx(url)
+    message_lable['text'] = f"Auto warte jest {price}zł"
 
 
 # Beginning of window object
@@ -12,31 +16,40 @@ root = tk.Tk()
 
 # Window size and grid shape
 canvas = tk.Canvas(root, width=600, height=300)
-canvas.grid(columnspan=3, rowspan=3)
+canvas.grid(columnspan=3, rowspan=4)
 
-# # Logo - placeholder
-# # Open image file
-# logo = Image.open('logo.png')
-# # Convert to Tk Image
-# logo = ImageTk.PhotoImage(logo)
-# # Create a logo widget
-# logo_label = tk.Label(image=logo)
-# logo_label.image = logo
-# logo_label.grid(column=1, row=0)
-
-# Entry box
-entry = tk.Entry(root)
-canvas.create_window(300, 200, window=entry)
+# Logo - placeholder
+# Open image file
+logo = Image.open('swgr_logo.png')
+# Convert to Tk Image
+logo = ImageTk.PhotoImage(logo)
+# Create a logo widget
+logo_label = tk.Label(image=logo)
+logo_label.image = logo
+logo_label.grid(column=1, row=0)
 
 # Instructions
-instructions = tk.Label(root, text="Podaj link do ogłoszenia które chcesz wycenić.", font="Raleway")
-instructions.grid(columnspan=3, column=0, row=1)
+message = "Podaj link do ogłoszenia z otomoto.pl które chcesz wycenić."
+message_lable = tk.Label(root, text=message, font="Raleway")
+message_lable.grid(columnspan=3, column=0, row=1)
+
+# Entry box
+entry = tk.Entry(root, width=75)
+entry.grid(columnspan=3, column=0, row=2,)
+offer_url = entry.get()
 
 # "Wyceń" button
 appraise_text = tk.StringVar()
-appraise_btn = tk.Button(root, textvariable=appraise_text, command=lambda:test_function(), font="Raleway", bg="#20bebe", fg="white", height=2, width=15)
+appraise_btn = tk.Button(root,
+                         textvariable=appraise_text,
+                         command=button_function,
+                         font="Raleway",
+                         bg="#c2121b",
+                         fg="white",
+                         height=1,
+                         width=15)
 appraise_text.set("Wyceń")
-appraise_btn.grid(column=1, row=2)
+appraise_btn.grid(column=1, row=3)
 
 # End of Window object
 root.mainloop()
--- user_interface.py
+++ user_interface.py
@@ -2,18 +2,34 @@ from check_offer import get_offer_data_url
 import numpy as np
 import tensorflow as tf
 
-# Load trained model
-model = tf.keras.models.load_model('dnn_model.keras')
+
+def appraise_offer(offer_url):
+    """Returns an appraised value of a car based on a URL of an otomoto.pl sale offer."""
+    model = tf.keras.models.load_model('dnn_model.keras')
+    offer_data = get_offer_data_url(offer_url)
+    offer_data_array = np.array(offer_data)
+    prediction = model.predict(offer_data_array)
+    return prediction[0][0]
 
 
-# Input data for prediction
-offer_url = input("otomoto.pl offer URL:\n")
-offer_data = get_offer_data_url(offer_url)
-offer_data_array = np.array(offer_data)
+def appraise_offer_approx(offer_url):
+    """Returns an approximated, appraised value of a car based on a URL of an otomoto.pl sale offer."""
+    return int(round(appraise_offer(offer_url)))
 
-# Make a prediction based on acquired from otomoto.pl offer
-prediction = model.predict(offer_data_array)
-
-# Return the prediction
-print(prediction)
-
+# # Load trained model
+# model = tf.keras.models.load_model('dnn_model.keras')
+#
+#
+# # Input data for prediction
+# offer_url = input("otomoto.pl offer URL:\n")
+# offer_data = get_offer_data_url(offer_url)
+# offer_data_array = np.array(offer_data)
+#
+# # Make a prediction based on acquired from otomoto.pl offer
+# prediction = model.predict(offer_data_array)
+#
+# # Return the prediction
+# print(prediction)
+if __name__ == "__main__":
+    print(appraise_offer("https://www.otomoto.pl/osobowe/oferta/opel-corsa-opel-corsa-1-4-ID6Gvheg.html"))
+    print(appraise_offer_approx("https://www.otomoto.pl/osobowe/oferta/opel-corsa-opel-corsa-1-4-ID6Gvheg.html"))
