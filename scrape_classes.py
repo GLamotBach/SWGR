6fb5d562ff197ea5f6dac7ae49729a8c440e2b4a
Grzegorz Lamot-Bach <105417272+stockphotohacker@users.noreply.github.com>
2024-08-03 11:44:10 +0200

    Scraper update

--- scrape_classes.py
+++ scrape_classes.py
@@ -1,10 +1,10 @@
-parameters_class = "ooa-162vy3d e130ulp53"
-parameter_class = "e130ulp54 ooa-12b2ph5"
+parameters_class = "ooa-162vy3d eyfqfx03"
+parameter_class = "eyfqfx04 ooa-12b2ph5"
 parameter_value_text = "e4cq37s0 ooa-1pe3502 er34gjf0"
 parameter_value_link = "e4cq37s1 ooa-1ftbcn2"
 price_value = "offer-price__number e1mlrgts4 ooa-1jtct0k er34gjf0"
 features_section_id = "content-equipments-section"
-features_class = "e1ic0wg14 ooa-1i4y99d er34gjf0"
-offer_listing_class = "e1vic7eh9 ooa-1ed90th er34gjf0"
+features_class = "evespt84 ooa-1i4y99d er34gjf0"
+offer_listing_class = "ooa-yca59n efpuxbr0"
 
 # TODO: A function that confirmes if classes are up to date.
